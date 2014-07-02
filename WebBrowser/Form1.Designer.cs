f6e500ad9073856d8d80b8a74f588aaed14d84bf
Claudia Zil <czil@ic.uach.cl>
2014-07-02 15:06:58 -0400

    Subir

---
+++ WebBrowser/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace WebBrowserTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(178, 26);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(55, 26);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(55, 56);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(178, 53);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(318, 26);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(12, 111);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(879, 348);
+            this.webBrowser1.TabIndex = 6;
+            this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(903, 567);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+    }
+}
+
---
+++ WebBrowser/Form1.cs
@@ -0,0 +1,298 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using System.Threading;
+
+
+namespace WebBrowserTest
+{
+    public partial class Form1 : Form
+    {
+
+        StreamWriter srWrite = null;
+
+        string parag = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+
+            string fileNew = @"hurricaneIsaacN.txt";
+
+            this.webBrowser1.ScriptErrorsSuppressed = true;
+
+            this.webBrowser1.Navigate("www.noslang.com");
+
+            srWrite = new StreamWriter(fileNew, false, Encoding.Default);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            //this.textBox1.Text = "lol";
+            
+
+            string fileName = @"hurricaneIsaac.txt";
+
+            string myLine = null; 
+            
+            try{
+
+            using (StreamReader srRead = new StreamReader(fileName, System.Text.Encoding.Default))
+             {
+                 
+                 //StreamWriter srWriteDel = new StreamWriter(fileNewDel, false, Encoding.Default);
+
+               
+
+                 int count = 0;
+
+                 //while (count < 639)
+                 //{
+                 //    myLine = srRead.ReadLine();
+
+                 //    count++;
+                 //}
+
+
+                 while ((myLine = srRead.ReadLine()) != null) // && count != 5)
+                 {
+
+
+                     //myLine = srRead.ReadLine();
+
+                     this.webBrowser1.Document.GetElementById("p").SetAttribute("value", myLine);
+
+                     HtmlElementCollection el = this.webBrowser1.Document.GetElementsByTagName("input");
+
+                     HtmlElementCollection frm = this.webBrowser1.Document.GetElementsByTagName("form");
+
+                     foreach (HtmlElement btn in el)
+                     {
+                         if (btn.Name == "submit")
+                         {
+                             btn.InvokeMember("Click");
+
+                             //foreach (HtmlElement f in frm)
+                             //{
+                             //    if (f.Name == "TheForm")
+
+                             //        f.InvokeMember("submit");
+                             //}
+
+                         }
+
+                     }
+
+                     //while (this.webBrowser1.ReadyState != WebBrowserReadyState.Complete)
+                     //{
+
+                     //   System.Threading.Thread.Sleep(10000); 
+                     //}
+
+                     MessageBoxTemporal.Show(this.webBrowser1.ReadyState.ToString(), "tit", 5, false);
+                 }
+                         //count++;
+            }
+            }
+             
+            catch(NullReferenceException ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+                 
+        }
+     
+        
+        
+           
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+
+           
+            /*this.webBrowser1.Document.GetElementById("p").SetAttribute("value", textBox1.Text);
+
+            HtmlElementCollection el = this.webBrowser1.Document.GetElementsByTagName("form");
+
+            foreach (HtmlElement frm in el)
+            {
+                if (frm.Name == "TheForm")
+                {
+                    frm.InvokeMember("submit");
+                }
+
+            }
+
+            HtmlElementCollection elSpan = this.webBrowser1.Document.GetElementsByTagName("span");
+
+            this.textBox2.Text = elSpan.Count.ToString();
+
+            foreach (HtmlElement btn in elSpan)
+            {
+                this.textBox2.Text = btn.InnerHtml;
+
+            }*/
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //System.Threading.Thread.Sleep(10000);
+
+            this.button1.PerformClick();
+
+            System.Threading.Thread.Sleep(10000);
+
+          
+
+            System.Threading.Thread.Sleep(10000);
+
+            this.button2.PerformClick();
+        }
+
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            
+            HtmlElementCollection elSpan = this.webBrowser1.Document.GetElementsByTagName("span");
+
+            this.textBox2.Text = elSpan.Count.ToString();
+
+            foreach (HtmlElement btn in elSpan)
+            {
+                //this.textBox2.Text = btn.InnerHtml;
+
+                    if (parag != btn.InnerHtml)
+                    {
+                        srWrite.WriteLine(btn.InnerHtml);
+
+                        parag = btn.InnerHtml;
+                    }
+
+            }
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (srWrite != null)
+            {
+                srWrite.Close();
+            }
+        }
+
+
+        }
+
+
+
+
+
+    public class MessageBoxTemporal
+    {
+        System.Threading.Timer IntervaloTiempo;
+        string TituloMessageBox;
+        string TextoMessageBox;
+        int TiempoMaximo;
+        IntPtr hndLabel = IntPtr.Zero;
+        bool MostrarContador;
+
+        MessageBoxTemporal(string texto, string titulo, int tiempo, bool contador)
+        {
+            TituloMessageBox = titulo;
+            TiempoMaximo = tiempo;
+            TextoMessageBox = texto;
+            MostrarContador = contador;
+
+            if (TiempoMaximo > 99) return; //Máximo 99 segundos
+            IntervaloTiempo = new System.Threading.Timer(EjecutaCada1Segundo,
+                null, 1000, 1000);
+            if (contador)
+            {
+                DialogResult ResultadoMensaje = MessageBox.Show(texto + "\r\nEste mensaje se cerrará dentro de " +
+                    TiempoMaximo.ToString("00") + " segundos ...", titulo);
+                if (ResultadoMensaje == DialogResult.OK) IntervaloTiempo.Dispose();
+            }
+            else
+            {
+                DialogResult ResultadoMensaje = MessageBox.Show(texto + "...", titulo);
+                if (ResultadoMensaje == DialogResult.OK) IntervaloTiempo.Dispose();
+            }
+        }
+        public static void Show(string texto, string titulo, int tiempo, bool contador)
+        {
+            new MessageBoxTemporal(texto, titulo, tiempo, contador);
+        }
+        void EjecutaCada1Segundo(object state)
+        {
+            TiempoMaximo--;
+            if (TiempoMaximo <= 0)
+            {
+                IntPtr hndMBox = FindWindow(null, TituloMessageBox);
+                if (hndMBox != IntPtr.Zero)
+                {
+                    SendMessage(hndMBox, WM_CLOSE, IntPtr.Zero, IntPtr.Zero);
+                    IntervaloTiempo.Dispose();
+                }
+            }
+            else if (MostrarContador)
+            {
+                // Ha pasado un intervalo de 1 seg:
+                if (hndLabel != IntPtr.Zero)
+                {
+                    SetWindowText(hndLabel, TextoMessageBox +
+                        "\r\nEste mensaje se cerrará dentro de " +
+                        TiempoMaximo.ToString("00") + " segundos");
+                }
+                else
+                {
+                    IntPtr hndMBox = FindWindow(null, TituloMessageBox);
+                    if (hndMBox != IntPtr.Zero)
+                    {
+                        // Ha encontrado el MessageBox, busca ahora el texto
+                        hndLabel = FindWindowEx(hndMBox, IntPtr.Zero, "Static", null);
+                        if (hndLabel != IntPtr.Zero)
+                        {
+                            // Ha encontrado el texto porque el MessageBox
+                            // solo tiene un control "Static".
+                            SetWindowText(hndLabel, TextoMessageBox +
+                                "\r\nEste mensaje se cerrará dentro de " +
+                                TiempoMaximo.ToString("00") + " segundos");
+                        }
+                    }
+                }
+            }
+        }
+        const int WM_CLOSE = 0x0010;
+        [System.Runtime.InteropServices.DllImport("user32.dll", SetLastError = true)]
+        static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+        [System.Runtime.InteropServices.DllImport("user32.dll",
+            CharSet = System.Runtime.InteropServices.CharSet.Auto)]
+        static extern IntPtr SendMessage(IntPtr hWnd, UInt32 Msg, IntPtr wParam, IntPtr lParam);
+        [System.Runtime.InteropServices.DllImport("user32.dll", SetLastError = true,
+            CharSet = System.Runtime.InteropServices.CharSet.Auto)]
+        static extern IntPtr FindWindowEx(IntPtr hwndParent, IntPtr hwndChildAfter,
+            string lpszClass, string lpszWindow);
+        [System.Runtime.InteropServices.DllImport("user32.dll", SetLastError = true,
+            CharSet = System.Runtime.InteropServices.CharSet.Auto)]
+        static extern bool SetWindowText(IntPtr hwnd, string lpString);
+    }
+    }
+
